92b8bcd15ea12b46a5cd41a8c6c110b832ce13c5
SBitong <stianbitong69@gmail.com>
2018-11-23 09:49:01 +0800

    Added Files
    Added Calculations.cs, Person.cs, Product.cs

---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleInitial;
+        public string LastName;
+        public string BirthDate;
+        public string Address;
+
+        public Person()
+        {
+
+        }
+
+        static string GetFullName()
+        {
+            return null;
+        }
+
+        static int GetAge()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public string Price;
+        public string Manufacturer;
+        public int Items;
+
+        public Product()
+        {
+
+        }
+
+        static void DeductItems(int items)
+        {
+            
+        }
+    }
+}
